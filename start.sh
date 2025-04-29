b0bc0904524d8765f31dafdf1eae8d993d0bf12c
Craig Ferguson <craig@tworing.ca>
2025-04-29 16:37:20 -0700

    VNC kind of working.  Run commands in start.sh individually

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Start virtual desktop
+Xvfb :1 -screen 0 1024x768x16 &
+export DISPLAY=:1
+
+# Start window manager
+startlxde &
+
+# Start VNC server (no password)
+x11vnc -display :1 -forever -nopw -shared -bg
+
+# Start noVNC
+websockify --web=/opt/novnc 6080 localhost:5900 &
+
+sleep 2
+
+# Launch Cataclysm (tiles)
+cd /workspace/src
+./cataclysm-tiles --basepath ./ --userdir /workspace/saves
